972126ec46f53fa927353b1d293212308d301801
Kevin Jordan <k.dean.jordan@gmail.com>
2020-08-12 06:04:59 -0700

    initial  commit

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 </template>
 
 <style lang="scss">
+@import './scss/_variables.scss';
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,7 +21,7 @@
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $red;
     &.router-link-exact-active {
       color: #42b983;
     }
