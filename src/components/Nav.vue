578c79d22a147175345da0c15923de3faf6b777a
Kevin Jordan <k.dean.jordan@gmail.com>
2020-08-14 15:16:50 -0700

    FEAT : nav working needs dropdowns

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,56 +1,60 @@
 <template>
     <div class="nav">
-        <div class="nav-container">
-            <div class="nav-logo">
-                <a href="/">
-                    <img class="nav-logo__svg" src="../assets/nav/logo.svg" alt="">
-                </a>
-            </div>
-            <div class="nav-menu__container">
-                <nav class="nav-menu__upper">
-                        <router-link class="nav-link nav-link__upper" to="/search">
-                            <span class="nav-link__upper--container">
-                                <div class="nav-link__upper--text">Search</div>  
-                                <img class="icon" src="../assets/nav/search.svg" alt="">
-                            </span>
-                        </router-link>
-                        <router-link class="nav-link nav-link__upper" to="/payments">
-                        <span class="nav-link__upper--container">
-                            <div class="nav-link__upper--text">Payments</div>  
-                            <img class="icon" src="../assets/nav/payments.svg" alt="">
-                        </span>
-                        </router-link>
-                        <router-link class="nav-link nav-link__upper" to="/account">
-                        <span class="nav-link__upper--container">
-                            <div class="nav-link__upper--text">Account</div>  
-                            <img class="icon" src="../assets/nav/account.svg" alt="">
-                        </span>
-                        </router-link>
-                        <router-link class="nav-link nav-link__upper" to="/cart">
-                        <span class="nav-link__upper--container">
-                            <div class="nav-link__upper--text">Cart</div>  
-                            <img class="icon" src="../assets/nav/cart.svg" alt="">
-                        </span>    
-                        </router-link>
-                </nav>
-                <nav class="nav-menu__lower">
-                    <span class="vert-border ghost"></span>
-                    <router-link class="nav-link nav-link__lower" to="/about">About</router-link>
-                    <span class="vert-border"></span>
-                    <router-link class="nav-link nav-link__lower" to="/tariffs">Tariffs</router-link>
-                    <span class="vert-border"></span>
-                    <router-link class="nav-link nav-link__lower" to="/subscribers">Subscribers</router-link>
-                    <span class="vert-border"></span>
-                    <router-link class="nav-link nav-link__lower" to="/partnerships">Partnerships</router-link>
-                    <span class="vert-border"></span>
-                    <router-link class="nav-link nav-link__lower" to="/contacts">Contact</router-link>
-                    <span class="vert-border ghost"></span>
-                </nav>
-            </div>
+        <div class="top-bar"></div>
+        <div class="bot-bar"></div>
+        <div class="nav-logo">
+            <a href="/"><img class="nav-logo__svg" src="../assets/nav/logo.svg" alt=""></a>
         </div>
-        
-    
-  </div>
+        <nav class="nav-menu nav-menu__upper">
+            <router-link class="nav-link nav-link__upper" to="/search">
+                <span class="nav-link__upper--container">
+                    <div class="nav-link__upper--text">Search</div>  
+                    <img class="icon" src="../assets/nav/search.svg" alt="">
+                </span>
+            </router-link>
+            <router-link class="nav-link nav-link__upper" to="/payments">
+            <span class="nav-link__upper--container">
+                <div class="nav-link__upper--text">Add Funds</div>  
+                <img class="icon" src="../assets/nav/payments.svg" alt="">
+            </span>
+            </router-link>
+            <router-link class="nav-link nav-link__upper" to="/account">
+            <span class="nav-link__upper--container">
+                <div class="nav-link__upper--text">My V-tell</div>  
+                <img class="icon" src="../assets/nav/account.svg" alt="">
+            </span>
+            </router-link>
+            <router-link class="nav-link nav-link__upper" to="/cart">
+            <span class="nav-link__upper--container">
+                <div class="nav-link__upper--text">Cart</div>  
+                <img class="icon" src="../assets/nav/cart.svg" alt="">
+            </span>    
+            </router-link>
+        </nav>
+        <nav class="nav-menu nav-menu__lower">
+            <span class="nav-link__upper--container wide">
+                <router-link class="nav-link nav-link__lower" to="/about">About</router-link>
+            </span>
+            <div class="vert-border"></div>
+            <span class="nav-link__upper--container wide">
+                <router-link class="nav-link nav-link__lower" to="/tariffs">Tariffs</router-link>
+            </span>
+            <div class="vert-border"></div>
+            <span class="nav-link__upper--container wide">
+                <router-link class="nav-link nav-link__lower" to="/subscribers">Subscribers</router-link>
+            </span>
+            <div class="vert-border"></div>
+            <span class="nav-link__upper--container wide">
+                <router-link class="nav-link nav-link__lower" to="/partnerships">Partnerships</router-link>
+            </span>
+            <div class="vert-border"></div>
+            <span class="nav-link__upper--container wide">
+                <router-link class="nav-link nav-link__lower" to="/contacts">Contact</router-link>
+            </span>
+        </nav>
+      
+            
+    </div>
 </template>
 
 <script>
@@ -63,41 +67,72 @@ export default {
 @import '../scss/_variables.scss';
 
 .nav {
-    position: fixed;
-    width: 100%;
+    position: sticky;
     top: 0;
-    left: 0;
-    width: 100%;
-    height: 4.5%;
-    background: #0d0d0d;
-
-    &::after {
-        position: fixed;
-        top: 4.5%;
-        height: 4.5%;
-        content: "";
-        background: rgba(0, 0, 0, 0.5);
-        display: block;
-        width: 100%;
-        z-index: -1;
-    }
+    right: 0;
 }
 
-.nav-container {
-    max-width: 1024px;
-    display: flex;
-    z-index: 2;
-    margin: 1rem auto;
+.top-bar {
+    
+    background-color: #0d0d0d;
+    height: 3.5rem;
+    display: block;
+    width: 100%;
+}
+.bot-bar {
+    background-color: rgba(0,0,0, .5);
+    height: 3rem;
+    display: block;
+    width: 100%;
+}
 
-    // border: 1px solid red;
+.nav-menu {
+    font-size: 14px;
+    line-height: 30px;
+    
+    // &__container {
+    //     display: flex;
+    //     flex-direction: column;
+    //     align-items: center;
+    //     justify-content: center;
+    //     color: white;
+    //     width: 830px;
+    // }
+
+    &__upper {
+        // margin-top: -3rem;
+        // border: 1px solid green;
+        position: absolute;
+        top: 9%;
+        left: 24%;
+        width: 65%;
+        max-width: 1024px;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+    }
+
+    &__lower {
+        position: absolute;
+        top: 54%;
+        left: 25%;
+        width: 60%;
+        max-width: 1024px;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
 }
 
 .nav-logo {
-    width: 16%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    
+    position: absolute;
+    top: 12%;
+    left: 15%;
+    width: 10%;    
     transition: all .4s;
 
     &__svg {
@@ -110,11 +145,11 @@ export default {
 }
 
 .vert-border {
-    display: inline-block;
-    background: red;
     width: 1px;
-    height: 50%;
-    transform: translateY(40%);
+    height: 30px;
+    background: red
+    // height: 50%;
+    // transform: translateY(40%);
 }
 
 .nav-link {
@@ -140,61 +175,30 @@ export default {
         }
 
         &--container {
+            // border: 1px solid green;
             width: 100%;
             display: flex;
             align-items: center;
             justify-content: center;
         }
     }
+}
+.wide {
+    width: 100%;
+    padding: 1%  0;
 
-    &__lower {
-        padding: 10px 10px;
-
-        &:hover {
-            background: red;
-        }
+    &:hover {
+        background: red;
     }
 }
 
-.nav-menu {
-    position: fixed;
-    width: 80%;
-    
-    &__container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        color: white;
-        width: 830px;
-    }
-
-    &__upper {
-        // padding-top: 1%;
-        width: 100%;
-        display: flex;
-        
-        // align-items: center;
-        // justify-content: center;
-        
-    }
-
-    &__lower {
-        width: 100%;
-        padding-top: 2.5%;
-        // border: 1px solid yellow;
-        display: flex;
-        justify-content: space-evenly;
-    }
-
-}
 
 .ghost {
     background: transparent;
 }
 
 .icon {
-    width: 15%;
-    margin-left: 5%;
+    width: 12%;
+    margin-left: 10%;
 }
 </style>
