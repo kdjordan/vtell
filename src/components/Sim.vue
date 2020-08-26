892e7ee53d17e34965e431e1849b0b710d542846
Kevin Jordan <k.dean.jordan@gmail.com>
2020-08-26 10:17:24 -0700

    REF: sim section sim section mobile done

--- src/components/Sim.vue
+++ src/components/Sim.vue
@@ -29,34 +29,38 @@
                  <div class="sim__steps-container--desc-large">V-Tell SIM card can be purchased at V-Tell sales offices and from V-Tell partners in Russia and abroad.</div>
              </div>
              <div v-else class="sim__steps-container pt-2">
-                 <div class="sim__steps-container--left flex-column">
-                     <img v-if="stage == 2" src="../assets/SVG/steps/phone-step.svg" alt="">
-                     <img v-else src="../assets/SVG/steps/step1.svg" alt="">
-                     <div class="sim__steps-container--step mb-1">STEP 1</div>
-                     <div class="sim__steps-container--step mb-2"><strong>ORDER ONLINE</strong></div>
+                 <div class="sim__steps-container--left">
+                     <div class="mobile-column">
+                        <img v-if="stage == 2" src="../assets/SVG/steps/phone-step.svg" alt="">
+                        <img v-else src="../assets/SVG/steps/step1.svg" alt="">
+                        <div class="sim__steps-container--step mb-1">STEP 1</div>
+                     </div>
+                     <div class="mobile-column__bottom">
+                        <div class="sim__steps-container--step mb-2"><strong>ORDER ONLINE</strong></div>
+                        <div class="sim__steps-container--desc">Choose a tariff, pick a number combination and place an order.</div>
+                     </div>
                  </div>
-                 <div class="sim__steps-container--center flex-column">
-                     <img src="../assets/SVG/steps/step2.svg" alt="">
-                     <div class="sim__steps-container--step mb-1">STEP 2</div>
-                     <div class="sim__steps-container--step mb-2"><strong>DELIVERY</strong></div>
+                 <div class="sim__steps-container--center">
+                     <div class="mobile-column">
+                        <img src="../assets/SVG/steps/step2.svg" alt="">
+                        <div class="sim__steps-container--step mb-1">STEP 2</div>
+                     </div>
+                     <div class="mobile-column__bottom">
+                        <div class="sim__steps-container--step mb-2"><strong>DELIVERY</strong></div>
+                        <div class="sim__steps-container--desc">Choose a delivery method: courier service or pick-up.</div>
+                     </div>
                  </div>
-                 <div class="sim__steps-container--right flex-column">
-                     <img src="../assets/SVG/steps/step3.svg" alt="">
-                     <div class="sim__steps-container--step mb-1">STEP 3</div>
-                     <div class="sim__steps-container--step mb-2 bold"><strong>SIM REGISTRATION</strong></div>
+                 <div class="sim__steps-container--right">
+                     <div class="mobile-column">
+                        <img src="../assets/SVG/steps/step3.svg" alt="">
+                        <div class="sim__steps-container--step mb-1">STEP 3</div>
+                     </div>
+                     <div class="mobile-column__bottom">
+                        <div class="sim__steps-container--step mb-2 bold"><strong>SIM REGISTRATION</strong></div>
+                        <div class="sim__steps-container--desc">Fill in required form fields to proceed with SIM card registration.</div>
+                     </div>
                  </div>
              </div>
-            <div v-if="stage == 1 || stage == 2" class="sim__desc-container">
-                <div class="sim__desc-container--left">
-                    <div class="sim__steps-container--desc">Choose a tariff, pick a number combination and place an order.</div>
-                </div>
-                <div class="sim__desc-container--middle">
-                    <div class="sim__steps-container--desc">Choose a delivery method: courier service or pick-up.</div>
-                </div>
-                <div class="sim__desc-container--right">
-                    <div class="sim__steps-container--desc">Fill in required form fields to proceed with SIM card registration.</div>
-                </div>
-            </div>
             <div class="lower-btn__container">
                 <button class="btn__sim btn__sim--red mt-3 mb-3">Buy now</button>
             </div>
@@ -116,13 +120,6 @@ export default {
     }
 }
 
-.flex-column {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-}
-
 .sim {
     display: flex;
     justify-content: center;
@@ -161,22 +158,64 @@ export default {
         display: flex;
         justify-content: space-between;
         align-content: center;
-        max-width: 690px;
+        width: 100%;
+        padding: 0 2rem;
         margin: 0 auto;
 
-        @media(max-width: 800px) {
-           
+        @media(max-width: 623px) {
+            flex-direction: column;
         }
+
+        & > div {
+            flex-direction: row;
+            width: 30%;
+
+        }
+
+        @media(max-width: 623px) {
+            & > div {
+                width: 100%;
+                display: flex;
+                margin-bottom: 2rem;
+            }
+        }
+
+        &--step {
+            @media(max-width: 623px) {
+                margin-bottom: 1rem;
+            }
+        }
+
+        & .mobile-column {
+            @media(max-width: 623px) {
+                display: flex;
+                flex-direction: column;
+                margin: 0 2rem;
+
+                &__bottom {
+                    text-align: left;
+                }
+            }
+        }
+        
 
         & img {
             margin-bottom: 3rem;
             width: 65px;
+
+            @media(max-width: 623px) {
+                margin-bottom: 1rem;
+            }
         }
 
         &--desc {
             font-size: 13px;
             // line-height: 2px;
             text-align: center;
+
+            @media(max-width: 623px) {
+                text-align: left;
+            }
 
             &-large {
                 font-size: 16px;
@@ -196,10 +235,14 @@ export default {
 
 }
 
-.lower-btn{
+.lower-btn {
     &__container {
         width: 25%;
         margin: 0 auto;
+
+        @media(max-width: 623px) {
+            width: 100%;
+        }
     }
 }
 
