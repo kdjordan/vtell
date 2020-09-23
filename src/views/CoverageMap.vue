85f1ce9f9e5ed558d9b2f69b56e04082ffe7e82b
Kevin Jordan <k.dean.jordan@gmail.com>
2020-09-22 19:48:20 -0700

    FEAT: coverage map finished civ map - working on modal pop

--- src/views/CoverageMap.vue
+++ src/views/CoverageMap.vue
@@ -4,34 +4,45 @@
         <PageCrumbs path="Coverage Map" />
         <PageDivider title="Coverage Map" />
         <div class="coverage__top-bar">
-           <div class="coverage__top-bar--indicator">HOME ZONE</div>
-           <div class="coverage__top-bar--search"><font-awesome-icon :icon="['fa', 'search']" /></div>
+           <div class="coverage__top-bar--indicator" :class="{blueHighlight: mapIndex == 2, redHighlight: mapIndex == 3}">
+               <span v-if="mapIndex ==1">HOME ZONE</span>
+               <span v-if="mapIndex ==2">ADDITIONAL PHONE NUMBERS TO ADD THEM TO YOUR PACKAGE</span>
+               <span v-if="mapIndex ==3">COUNTRIES WHERE V-TELL WORKS</span>
+               
+            </div>
+           <div class="coverage__top-bar--search" :class="{blueHighlight: mapIndex == 2, redHighlight: mapIndex == 3}"><font-awesome-icon :icon="['fa', 'search']" /></div>
         </div>
+        <div v-if="modalActive" class="country-modal">CONTENT</div>
         <div class="coverage__map-container">
             <svg @click="mapShift(0)" class="coverage__arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26.68 78.47">
-                <path d="M0,39.1,22.29.5A1,1,0,0,1,23.65.13l2.53,1.46A1,1,0,0,1,26.55,3L5.61,39.23,26.55,75.51a1,1,0,0,1-.37,1.36l-2.53,1.46A1,1,0,0,1,22.29,78L0,39.36" fill="#159846"/>
+                <path d="M0,39.1,22.29.5A1,1,0,0,1,23.65.13l2.53,1.46A1,1,0,0,1,26.55,3L5.61,39.23,26.55,75.51a1,1,0,0,1-.37,1.36l-2.53,1.46A1,1,0,0,1,22.29,78L0,39.36" 
+                fill="#159846" :class="{blueHighlight: mapIndex == 2, redHighlight: mapIndex == 3}"/>
             </svg>
+            
             <div class="coverage__map-container--inner">
                 <Map :mapIndex="`${mapIndex}`"/>
-
             </div>
             <svg @click="mapShift(1)" class="coverage__arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26.68 78.47">
-                <path d="M26.68,39.1,4.4.5A1,1,0,0,0,3,.13L.5,1.59A1,1,0,0,0,.13,3l21,36.27L.13,75.51A1,1,0,0,0,.5,76.87L3,78.33A1,1,0,0,0,4.4,78L26.68,39.36" fill="#159846"/>
+                <path d="M26.68,39.1,4.4.5A1,1,0,0,0,3,.13L.5,1.59A1,1,0,0,0,.13,3l21,36.27L.13,75.51A1,1,0,0,0,.5,76.87L3,78.33A1,1,0,0,0,4.4,78L26.68,39.36" 
+                fill="#159846" :class="{blueHighlight: mapIndex == 2, redHighlight: mapIndex == 3}"/>
             </svg>
        </div>
        <div class="coverage__indicators">
-           <span :class="{activeMap: mapIndex == 1}"></span>
-           <span :class="{activeMap: mapIndex == 2}"></span>
-           <span :class="{activeMap: mapIndex == 3}"></span>
+           <span :class="{activeMap: mapIndex == 1}" @click.stop="mapIndex = 1"></span>
+           <span :class="{activeMapBlue: mapIndex == 2}" @click.stop="mapIndex = 2"></span>
+           <span :class="{activeMapRed: mapIndex == 3}" @click.stop="mapIndex = 3"></span>
        </div>
        <div class="coverage__countries">
-           <p>Austria Belgium Brazil Bulgaria Canada Chech Republic Croatia Cyprus Denmark Dominican Republic EgyptF rance French Guiana Germany Gibraltar Greece Guadeloupe Hungary Iceland Ireland Israel Italy Latvia Liechtenstein Lithuania Luxembourg Malta Martinique Mexico Monaco Netherlands Norway Poland Portugal Puerto Rico RussiaS ingapore SlovakiaSouth Korea Spain Sweden SwitzerlandU nited KingdomU nited States of America United States Virgin Islands</p> 
+           <p v-if="mapIndex == 1"><span class="block block-green"></span>Austria Belgium Brazil Bulgaria Canada Chech Republic Croatia Cyprus Denmark Dominican Republic Egypt France French Guiana Germany Gibraltar Greece Guadeloupe Hungary Iceland Ireland Israel Italy Latvia Liechtenstein Lithuania Luxembourg Malta Martinique Mexico Monaco Netherlands Norway Poland Portugal Puerto Rico RussiaS ingapore SlovakiaSouth Korea Spain Sweden Switzerland United Kingdom Uited States of America United States Virgin Islands</p> 
+           <p v-if="mapIndex == 2"><span class="block block-blue"></span>Albania Angola Argentina Australia Austria Belarus Belgium Brazil Bulgaria Cambodia Canada Cayman Islands Chech Republic China Colombia Costa Rica Croatia Cyprus Denmark Dominican Republic El Salvador Estonia Finland  France Georgia Germany Ghana Greece Grenada Guatemala Hong Kong Hungary Iceland Ireland Israel Italy Jamaica Japan Kazakhstan Kenya Latvia Lithuania Luxembourg Macao Malaysia Malta Mexico Netherlands New Zealand Nigeria Norway Panama Peru Philippines Poland Portugal Puerto Rico Romania Russia Singapore Slovakia Slovenia South Africa South Korea Spain Sweden Switzerland Thailand Trinidad and Tobago Turkey Ukraine United KingdomUnited States of America Venezuela Vietnam</p> 
+           <p v-if="mapIndex == 3"><span class="block block-red"></span>Afghanistan Albania Algeria Andorra Angola Anguilla Antigua and Barbuda Argentina Armenia Aruba Australia Austria Azerbaijan Bahamas Bahrain Bangladesh Barbados Belarus Belgium Belize Bermuda Bhutan Bolivia Bosnia and Herzegovina Botswana Brazil British Virgin Islands Brunei Bulgaria Burkina Faso Burundi Cabo Verde Cambodia Cameroon Canada Cayman Islands Central African Republic Chad Chech Republic Chile China Colombia Comoros Costa Rica Cote d`Ivoire Croatia Cuba Cyprus Democratic Republic of the Congo Denmark Djibouti Dominican Republic Dominika Ecuador Egypt El Salvador Equatorial Guinea Estonia Eswatini (Swaziland) Ethiopia Faroe Islands Fiji Finland France French Guiana French Polynesia Gabon Gambia Georgia Germany Ghana Gibraltar Greece Grenada Guadeloupe Guam Guatemala Guernsey Guinea Guinea-Bissau Guyana Haiti Honduras Hong Kong Hungary Iceland India Indonesia Iran Iraq Ireland Isle of Man Israel Italy Jamaica Japan Jersey Jordan Kazakhstan Kenya Kiribati Kosovo Kuwait Kyrgyzstan Laos Latvia Lebanon Lesotho Liberia Libya Liechtenstein Lithuania Luxembourg Macao Madagascar Malawi Malaysia Mali Malta Martinique Mauritania Mauritius Mayotte Mexico Moldova Monaco Mongolia Montenegro Montserrat Morocco Mozambique Myanmar Namibia Nauru Nepal Netherlands Netherlands Antilles (Saba, Curacao, Bonaire, Sint Eustatius, Saint Martin) New Zealand Nicaragua Niger Nigeria North Macedonia Northen Mariana Islands Norway Oman Pakistan Palestine State Panama Papua New Guinea Paraguay Peru Philippines Poland Portugal Puerto Rico Qatar Republic of the CongoReunion (La) Romania Russia Rwanda Republic of Saint Barthélemy (French West Indies) Saint Kitts and Nevis Saint Lucia Saint Martin (French West Indies)Saint Vincent and the Grenadines Samoa Saudi Arabia Senegal Serbia Seychelles Sierra Leone Singapore Slovakia Slovenia Somalia South Africa South Korea South Sudan Spain Sri Lanka Sudan Suriname Sweden Switzerland Syria Taiwan Tajikistan Tanzania Thailand Timor-Leste Togo Trinidad and Tobago Tunisia Turkey Turkmenistan Turks and Caicos Islands Uganda Ukraine United Arab Emirates United Kingdom United States of America United States Virgin Islands Uruguay Uzbekistan Vanuatu Venezuela Vietnam Yemen Zambia Zimbabwe</p> 
        </div>
        <div class="coverage__desc">
            <p>V-Tell’s Roaming-Free Mobile Systems have dissolved country borders, allowing V-Tell Subscribers to make calls, access internet and send SMS messages from any part of the world, as if they were in their home country. A solo V-Tell SIM Card allows Subscribers to have an unlimited amount of permanently active phone numbers from over 77 countries. V-Tell’s quality of service is guaranteed by its own personal telecommunication infrastructure and partnerships with world’s leading mobile network operators.</p>
            <p>For people who live outside their home country, V-Tell’s services bring unique values, compared to local mobile network operators. Relatives and friends located in different countries do not have to make international calls if the V-Tell Subscriber has activated those countries local phone numbers.</p>
            <p>Detailed information about tariffs you will find here.</p>
        </div>
+       
     </div>
 </div>
 
@@ -44,7 +55,8 @@ export default {
     components: {PageDivider, PageCrumbs, Map},
     data() {
         return {
-            mapIndex: 1
+            mapIndex: 1,
+            modalActive: true
         }
     },
     methods: {
@@ -75,6 +87,19 @@ export default {
 @import '../scss/_utilities.scss';
 @import '../scss/_page.scss';
 
+.country-modal {
+    color: white;
+    // padding: 2rem;
+    // position: relative;
+    font-size: 20px;
+    position: absolute;
+    top: 35%;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    padding: 1.5rem 10rem;
+    background: rgba(0, 152, 70, .8);
+}
+
 .coverage {
     &__arrow {
         fill: #ffffff !important;
@@ -102,6 +127,15 @@ export default {
         display: flex;
         align-items: center;
         justify-content: space-between;
+
+        & .coverage__arrow {
+            cursor: pointer;
+            transition: all .4s;
+            
+            &:hover {
+                filter: opacity(50%);
+            }
+        }
     }
     &__indicators {
         display: flex;
@@ -109,6 +143,7 @@ export default {
         align-items: center;
 
         & span {
+            cursor: pointer;
             display: block;
             height: 15px;
             width: 15px;
@@ -147,6 +182,42 @@ export default {
     background: rgb(0, 152, 70) !important;
     height: 25px !important;
     width: 25px !important;
+    &Blue {
+        background: rgb(76,134,227) !important;
+        height: 25px !important;
+        width: 25px !important;
+    }
+    &Red {
+        background: rgb(230, 57, 55) !important;
+        height: 25px !important;
+        width: 25px !important;
+    }
+}
 
+.blueHighlight {
+    fill: rgb(76,134,227);
+    color:  rgb(76,134,227) !important;
+}
+.redHighlight {
+    fill: rgb(230, 57, 55);
+    color: rgb(230, 57, 55) !important;
+}
+.block {                                                                    
+    display: inline-block;
+    margin-right: .5rem;
+    height: 12px !important;
+    width: 12px !important;
+
+    &-green {
+        background: rgb(0, 152, 70) !important;
+    }
+
+    &-blue {
+        background: rgb(76,134,227) !important;
+    }
+
+    &-red {
+        background: rgb(230, 57, 55) !important;
+    }
 }
 </style>
